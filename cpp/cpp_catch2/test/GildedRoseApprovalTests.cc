7608920f58b2cf37d8567336524dcf8452b63f59
Jacob Mossberg <jacob@jacobmossberg.se>
2019-12-03 16:55:52 +0100

    [cpp_catch2] refactor GildedRoseApprovalTests.cc

--- cpp/cpp_catch2/test/GildedRoseApprovalTests.cc
+++ cpp/cpp_catch2/test/GildedRoseApprovalTests.cc
@@ -16,12 +16,14 @@ TEST_CASE("GildedRoseApprovalTests", "VerifyCombinations")
     std::vector<int> sellIns { 1 };
     std::vector<int> qualities { 1 };
 
+    auto f = [](string name, int sellIn, int quality) {
+        vector<Item> items = {Item(name, sellIn, quality)};
+        GildedRose app(items);
+        app.updateQuality();
+        return items[0];
+    };
+
     ApprovalTests::CombinationApprovals::verifyAllCombinations(
-            [](string name, int sellIn, int quality) {
-                vector<Item> items = {Item(name, sellIn, quality)};
-                GildedRose app(items);
-                app.updateQuality();
-                return items[0];
-            },
+            f,
             names, sellIns, qualities);
 }
